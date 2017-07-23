ff1e094df8575966f30d1a68fadc5d1ac4c6fc0c
MaYiLagann <lhwshy@naver.com>
2017-07-23 15:26:08 +0900

    시스템 구현
    + 시간 흐름 구현
    + 시간 표시 UI 추가
    + 이벤트 시스템 구현

---
+++ Assets/2. Scripts/EventCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventCtrl : MonoBehaviour {
+	
+	[Header("Event Settings")]
+	public string EventName;
+	[TextArea(3, 8)]
+	public string EventText;
+	public int EventKind;
+
+	[Header("Active Time Settings")]
+	public int ActiveDay = 1;
+	public int ActiveMonth = 1;
+	public int ActiveYear = 1;
+
+	private GameSystemCtrl GameSystem;
+
+	void Awake() {
+		GameSystem = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameSystemCtrl> ();
+		StartCoroutine (AddEvent ());
+	}
+
+	void Start () {
+		
+		if (ActiveDay <= 0 || ActiveMonth <= 0) {
+			Debug.LogError ("Active time can't set under 0");
+			return;
+		}
+
+		switch(EventKind){
+		default:
+			Debug.Log ("Event Actived, Event Kind: " + EventKind);
+			break;
+		}
+
+		Destroy (gameObject);
+	}
+
+	IEnumerator AddEvent(){
+		yield return new WaitForEndOfFrame ();
+		GameSystem.AddEvent (this);
+	}
+
+}
---
+++ Assets/2. Scripts/GameSystemCtrl.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSystemCtrl : MonoBehaviour { // This object tag must be "GameController"
+
+	[Header("Game Settings")]
+	public float AddTimeDelay = 1f;
+
+	[Header("Time Settings")]
+	public int StartDay = 1;
+	public int StartMonth = 1;
+	public int StartYear = 2017;
+
+	[Header("UI Settings")]
+	public Canvas UiCanvas;
+	public Text TimeText;
+
+	private int TimeDay;
+	private int TimeMonth;
+	private int TimeYear;
+
+	private List<EventCtrl> EventQueue;
+
+	/* Variables */
+
+	void Start () {
+		EventQueue = new List<EventCtrl>(100);
+		GameStart ();
+	}
+
+	/* Event Functions */
+
+	void GameStart () {
+		TimeDay = StartDay;
+		TimeMonth = StartMonth;
+		TimeYear = StartYear;
+		StartCoroutine (RunTime ());
+	}
+
+	IEnumerator RunTime () {
+		TimeText.text = TimeYear + "Y " + TimeMonth + "M " + TimeDay + "D";
+
+		yield return new WaitForSeconds (AddTimeDelay);
+
+		TimeDay++;
+
+		int maxDay = 0;
+		switch (TimeMonth) {
+		case 1:
+			maxDay = 31;
+			break;
+		case 2:
+			maxDay = 28;
+			break;
+		case 3:
+			maxDay = 31;
+			break;
+		case 4:
+			maxDay = 30;
+			break;
+		case 5:
+			maxDay = 31;
+			break;
+		case 6:
+			maxDay = 30;
+			break;
+		case  7:
+			maxDay = 31;
+			break;
+		case 8:
+			maxDay = 31;
+			break;
+		case 9:
+			maxDay = 30;
+			break;
+		case 10:
+			maxDay = 31;
+			break;
+		case 11:
+			maxDay = 30;
+			break;
+		case 12:
+			maxDay = 31;
+			break;
+		}
+
+		if (TimeDay > maxDay) {
+			TimeDay = 1;
+			if (TimeMonth == 12) {
+				TimeMonth = 1;
+				TimeYear++;
+			} else
+				TimeMonth++;
+		}
+
+		if (EventQueue.Count > 0 && TimeCheck (EventQueue [0].ActiveDay, EventQueue [0].ActiveMonth, EventQueue [0].ActiveYear)) {
+			EventQueue [0].enabled = true;
+			EventQueue.RemoveAt (0);
+		}
+
+		StartCoroutine (RunTime ());
+	}
+
+	public bool TimeCheck (int Day, int Month, int Year) {
+		return Day == TimeDay && Month == TimeMonth && Year == TimeYear;
+	}
+
+	public void AddEvent (EventCtrl e) {
+		EventQueue.Add (e);
+	}
+}
