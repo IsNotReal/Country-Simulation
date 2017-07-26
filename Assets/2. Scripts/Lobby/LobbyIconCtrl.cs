ac0b95f6628b9aeee4501f7c607cde1570c8079d
MaYiLagann <lhwshy@naver.com>
2017-07-27 00:02:42 +0900

    로비 구현
    + 메인 로비 구현 ( 로고, 설정 버튼, 랭킹 버튼, 플레이버튼 추가 )
    + 게임 플레이 로비 구현 ( 새 게임 버튼, 저장 불러오기 버튼 추가 )

---
+++ Assets/2. Scripts/Lobby/LobbyIconCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyIconCtrl : MonoBehaviour {
+
+	public Vector2 ShowTime;
+
+	private Animator thisAnim;
+	private bool isAnimRun = false;
+
+	void Start () {
+		thisAnim = gameObject.GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if (thisAnim.GetCurrentAnimatorStateInfo (0).IsName ("IdleState") && !isAnimRun) {
+			StartCoroutine (IconShow (Random.Range (ShowTime.x, ShowTime.y)));
+			isAnimRun = true;
+		}
+	}
+
+	IEnumerator IconShow(float time){
+		yield return new WaitForSeconds (time);
+		thisAnim.SetTrigger ("Start");
+		isAnimRun = false;
+	}
+}
---
+++ Assets/2. Scripts/Lobby/LobbyMenuCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyMenuCtrl : MonoBehaviour {
+
+	private int CurrentPage = 0;
+	private Animator thisAnim;
+
+	void Start () {
+		thisAnim = gameObject.GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && CurrentPage > 0) {
+			MoveAnim (-CurrentPage);
+			CurrentPage--;
+		}
+	}
+
+	public void MoveAnim(int num){
+		CurrentPage = num;
+		switch (num) {
+		case 1:
+			thisAnim.SetTrigger ("MoveF1");
+			break;
+		case -1:
+			thisAnim.SetTrigger ("BackF1");
+			break;
+		default:
+			Debug.LogError ("Wrong page select \n\rCheck button event");
+			break;
+		}
+	}
+}
