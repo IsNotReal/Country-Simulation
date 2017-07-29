cb03d09c67dc339d1ba951b1d95b8b12642c92f6
MaYiLagann <lhwshy@naver.com>
2017-07-29 17:33:04 +0900

    로비 구현 완료
    + 국가 설정 로비 구현 ( 디자인 리소스 필요 )
    + 게임 씬과 로비 씬 연결 완료
    + 국가 선택 버튼 누를 시 국가 설정 로비에서 국가 선택 로비로 돌아감

--- Assets/2. Scripts/Lobby/LobbyMenuCtrl.cs
+++ Assets/2. Scripts/Lobby/LobbyMenuCtrl.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LobbyMenuCtrl : MonoBehaviour {
 
+	public string GameSceneName = "GameScene";
+	public int MaxLobby;
+
+	[Header("Object Settings")]
 	public Animator ForegroundAnim;
+	public Text HeadText;
+	public Slider DifficultySlider;
+	public Text DifficultyPercentText;
 
 	private int CurrentPage = 0;
 	private Animator thisAnim;
@@ -21,8 +30,22 @@ public class LobbyMenuCtrl : MonoBehaviour {
 		}
 	}
 
+	void FixedUpdate () {
+		DifficultyPercentText.text = DifficultySlider.value + "%";
+	}
+
 	public void MoveAnim(int num){
+		
+		if (num > 0 && CurrentPage == 3) {
+			MoveScene ();
+			return;
+		}
+
+		if (CurrentPage == num)
+			return;
+		
 		CurrentPage = num;
+
 		if (num != 0) {
 			thisAnim.SetInteger ("Move", num);
 			thisAnim.SetTrigger ("Start");
@@ -31,5 +54,19 @@ public class LobbyMenuCtrl : MonoBehaviour {
 			ForegroundAnim.SetTrigger ("Hide");
 		if (num == 1)
 			ForegroundAnim.SetTrigger ("Show");
+		if (num == 3)
+			HeadText.text = "국가설정";
+		if (num == -3)
+			HeadText.text = "국가선택";
+		
+	}
+
+	public void MoveSelectCountry(){
+		if (CurrentPage == 3)
+			MoveAnim (-3);
+	}
+
+	void MoveScene() {
+		SceneManager.LoadScene (GameSceneName);
 	}
 }
